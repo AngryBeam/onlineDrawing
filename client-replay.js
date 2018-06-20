ed5f2e81a6abb22a547ddd99b98cff35f5cd985f
Paveen Asawasuebsakul <Paveen@Paveens-MacBook.local>
2018-06-20 21:19:33 +0700

    first commit

---
+++ client-replay.js
@@ -0,0 +1,76 @@
+$(function(){
+
+    // Configuration
+    //var url = 'http://127.0.0.1'; // URL of your webserver
+    var line_thickness = 7;
+    var line_colour = "blue";
+
+    // Variables
+    var canvas = $('#paper');
+    var ctx = canvas[0].getContext('2d');
+    //var id = Math.round($.now() * Math.random()); // Generate a unique ID
+    //var drawing = false; // A flag for drawing activity
+    //var touchUsed = false; // A flag to figure out if touch was used
+    var clients = {};
+    var cursors = {};
+    var prev = {}; // Previous coordinates container
+
+    var socket = io();
+    
+    // Drawing helper function
+    function drawLine(fromx, fromy, tox, toy)
+    {
+        ctx.lineWidth = line_thickness;
+        ctx.strokeStyle = line_colour;
+        ctx.lineCap = "round";
+        ctx.beginPath();
+        ctx.moveTo(fromx, fromy);
+        ctx.lineTo(tox, toy);
+        ctx.stroke();
+    }
+    
+    // Keep users screen up to date with other users cursors & lines
+   function rePlay(data) {
+        console.log('Render Replay');
+        console.log(data);
+        // Create cursor
+        if ( !(data.id in clients) )
+        {
+            cursors[data.id] = $('<div class="cursor">').appendTo('#cursors');
+        }
+        
+        // Move cursor
+        cursors[data.id].css({
+            'left' : data.x,
+            'top' : data.y
+        });
+        
+        // Set the starting point to where the user first touched
+        if (data.drawing && clients[data.id] && data.touch)
+        {
+            clients[data.id].x = data.startX;
+            clients[data.id].y = data.startY;
+        }
+
+        // Show drawing
+        if (data.drawing && clients[data.id])
+        {
+            // clients[data.id] holds the previous position of this user's mouse pointer
+            drawLine(clients[data.id].x, clients[data.id].y, data.x, data.y);
+        }
+        
+        // Save state
+        clients[data.id] = data;
+        clients[data.id].updated = $.now();
+    }
+
+    socket.on('replay', function (data) {
+        console.log('Replay data received.');
+        console.log(data);
+
+        data.forEach(element => {
+            //setInterval(rePlay(element), 2000);
+            setTimeout(rePlay(element), 8000);
+        });
+    });
+});
---
+++ server.js
@@ -0,0 +1,39 @@
+// Including libraries
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+var static = require('node-static'); // for serving files
+
+// This will make all the files in the current folder
+// accessible from the web
+var fileServer = new static.Server('./');
+	
+// This is the port for our web server.
+// you will need to go to http://localhost:8080 to see it
+const port = process.env.PORT || 8080;
+app.listen(port);
+
+// If the URL of the socket server is opened in a browser
+function handler(request, response) {
+	request.addListener('end', function () {
+		fileServer.serve(request, response);
+	}).resume();
+}
+
+var replayData = [];
+
+// Listen for incoming connections from clients
+io.sockets.on('connection', function (socket) {
+	// Listen for mouse move events
+	console.log('New Connection');
+	socket.on('mousemove', function (data) {
+		console.log(data);
+		replayData.push(data);
+		socket.broadcast.emit('moving', data); // Broadcasts event to everyone except originating client
+	});
+	socket.on('replay', function (data) {
+		console.log('Sending Replay Data');
+		//console.log(replayData);
+		socket.broadcast.emit('replay', replayData);
+		replayData = [];
+	})
+});
